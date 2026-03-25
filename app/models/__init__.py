6dd4c6e2f72fdf162d93f8621536f04485598329
Vinicius Resende <resende-vinicius@hotmail.com>
2026-03-24 23:09:21 -0300

    fix: corrige __init__ e estrutura de módulos

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.equipment import Equipment
-from app.models.work_order import WorkOrder
-from app.models.maintenance_history import MaintenanceHistory
